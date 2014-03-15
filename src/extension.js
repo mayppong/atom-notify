3c599389727920362a9e6b404ddc4e7f9aa557de
May P <mayppong@gmail.com>
2014-03-15 10:41:15 -0700

    Format and comment code.
    Add a bit of comments to the code. Nothing major. Mostly for educational
    purpose.

--- src/extension.js
+++ src/extension.js
@@ -1,13 +1,23 @@
+/**
+ * This is the main file used to load the extension, it must have
+ * init(), enable() and disable() methods provided below.
+ *
+ * TO-DO: break classes off to its own files
+ */
 
-const Lang = imports.lang;
-const Clutter = imports.gi.Clutter;
+const Lang      = imports.lang;
+const Clutter   = imports.gi.Clutter;
+const St        = imports.gi.St;
 
-const St = imports.gi.St;
-const Main = imports.ui.main;
-const Tweener = imports.ui.tweener;
+const Main      = imports.ui.main;
 const PanelMenu = imports.ui.panelMenu;
-const Panel = imports.ui.panel;
+const Panel     = imports.ui.panel;
 
+
+/**
+ * This class provides the icon in the system status area 
+ * and the menu.
+ */
 const NotificationCenter = new Lang.Class({
     Name:    'nos-notify/NotificationCenter',
     Extends:  PanelMenu.Button,
@@ -39,18 +49,31 @@ const NotificationCenter = new Lang.Class({
 });
 
 
+
 let button;
 
+/**
+ * init() is called at most once in a GNOME Shell session, 
+ * during the first-run before enable(). 
+ * A good place for global initialization. 
+ * Don't do any thing major like UI modification.
+ */
 function init() {
 
 }
 
+/**
+ * enable() is called when the user enables the extension.
+ */
 function enable() {
     button = new NotificationCenter();
 
     Main.panel.addToStatusArea('notifycenter', button);
 }
 
+/**
+ * disable() is called when the user disables the extension.
+ */
 function disable() {
     Main.panel._rightBox.remove_child(button);
 }
